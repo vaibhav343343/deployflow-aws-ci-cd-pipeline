02772db0ec2d1721c6fd6a05ea1af600b25d99dc
Ubuntu <ubuntu@ip-172-31-21-71.ec2.internal>
2026-03-20 07:55:05 +0000

    fix static error

--- app/main.py
+++ app/main.py
@@ -1,18 +1,13 @@
 from fastapi import FastAPI
 from fastapi.responses import FileResponse
-from fastapi.staticfiles import StaticFiles
 import os
 
 app = FastAPI()
-
-# static folder (future use ke liye - css/js/images)
-app.mount("/static", StaticFiles(directory="app/static"), name="static")
 
 @app.get("/")
 def home():
     file_path = "app/templates/index.html"
 
-    # safety check (agar file missing ho to error na aaye)
     if os.path.exists(file_path):
         return FileResponse(file_path)
     else:
